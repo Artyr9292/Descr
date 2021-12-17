b078c8409467fabd3f735f00404105576e0f8f9d
Artyr <arktik-1992@mail.ru>
2021-12-17 21:38:49 +0500

    Homework-2

---
+++ js/main.js
@@ -0,0 +1,47 @@
+//12.Практика,ч.1.Начинаем создавать приложение
+
+//Делаем домашку
+
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+//console.log('numberOfFilms');
+
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+//2 домашка
+//1 задание(цикл комбинируется с условием)
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    // 2 домашка/2задание
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+    // *********
+    
+}
+//2 домашка/3задание
+if (personalMovieDB.count < 10) {
+    console.log("Просмотров довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30){
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
+// ****************** //
+
+console.log(personalMovieDB);
