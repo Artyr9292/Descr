7eeb91b86239674033bf5a74f3bc6853c09e143b
Artyr <arktik-1992@mail.ru>
2021-12-28 04:20:55 +0500

    Homework_4

--- js/main.js
+++ js/main.js
@@ -3,80 +3,98 @@
 //Делаем домашку
 'use strict';
 
-let numberOfFilms; //глобальная переменная
+//let numberOfFilms; //глобальная переменная
 
 //18.Практика,ч.3.Используем функции.
 //1 домашка
-function start() {
-    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
 
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
-    }
-}
-
-start();
 
 // ************************************* //
 
 
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0, //изначально количество фильмов будет 0
     movies: {},
     actors: {},
     genres: [],
-    privat: false
-};
-
-
-//3домашка(практика ч.3) 1 задание
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) { //2 домашка.1 задание(цикл комбинируется с условием)
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
-              b = prompt('На сколько оцените его?', '');
+    privat: false,
+    start: function() { //можно применить стрелочную функцию
+        personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", '');
     
-        // 2 домашка/2задание
-        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-        } else {
-            console.log('error');
-            i--;
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", '');
         }
-        // *********  
-    }
-}
-//rememberMyFilms();
+    },
+    rememberMyFilms: function() {
+        for (let i = 0; i < 2; i++) { //2 домашка.1 задание(цикл комбинируется с условием)
+            const a = prompt('Один из последних просмотренных фильмов?', ''),
+                  b = prompt('На сколько оцените его?', '');
+        
+            // 2 домашка/2задание
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log('done');
+            } else {
+                console.log('error');
+                i--;
+            }
+            // *********  
+        }
+    },
+    detectPersonalLevel: function() {
+        //2 домашка/3задание
+      if (personalMovieDB.count < 10) {
+        console.log("Просмотров довольно мало фильмов");
+      } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Вы классический зритель");
+      } else if (personalMovieDB.count >= 30){
+        console.log("Вы киноман");
+      } else {
+        console.log("Произошла ошибка");
+      }
+    },
+    showMyDB: function(hidden) {
+        if (!hidden) { //если наша база данных скрыта то мы ее показываем
+            console.log(personalMovieDB);  
+        }
+    },
+    toggleVisibleMyDB: function() {
+        if(personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    },
+    writeYourGenres: function() {
+        for (let i = 1; i < 2; i++) {
+        //for (let i = 1; i <= 3; i++) {
+            /*
+            let genre = prompt(`Ваш любимый жанр под номером ${i}`);
 
-function detectPersonalLevel() {
-    //2 домашка/3задание
-  if (personalMovieDB.count < 10) {
-    console.log("Просмотров довольно мало фильмов");
-  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("Вы классический зритель");
-  } else if (personalMovieDB.count >= 30){
-    console.log("Вы киноман");
-  } else {
-    console.log("Произошла ошибка");
-  }
-}
-//detectPersonalLevel();
+            if (genre === ''  || genre == null) {
+                console.log('Вы ввели некорректные данные или неввели их вообще');
+                i--;
+            } else {
+                personalMovieDB.genres[i - 1] = genre;
+            }
+            */
 
-//3домашка(практика ч.3) 1 задание
-function showMyDB (hidden) {
-    if (!hidden) { //если наша база данных скрыта то мы ее показываем
-        console.log(personalMovieDB);  
-    }
-}
+            //2 вариант решения нашей задачки, той что выше(которая закомментирована)
+            let genres = prompt(`Введите ваши любимые жанры через запятую`).toLowerCase();
 
-showMyDB(personalMovieDB.privat);  
+            if (genres === ''  || genres == null) {
+                console.log('Вы ввели некорректные данные или неввели их вообще');
+                i--;
+            } else {
+                personalMovieDB.genres = genres.split(', ');
+                personalMovieDB.genres.sort();
+            } // введите в консоли разработчика в браузере personalMovieDB.writeYourGenres(); увидите 
+        }                    //как там отображаются наши данные
 
-//3домашка.3 задание.
-function writeYourGenres() {
-    for (let i = 1; i <= 3; i++) {
-        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
-    }
-}  
-writeYourGenres();
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i + 1} - это ${item}`);
+        }); //можем протестить выведя в консоли браузера personalMovieDB.writeYourGenres();
+    } // выведим в консоли personalMovieDB и увидим что все 3 свойства в браузере сохранились Array(3)
+};
 
 // ****************** //
